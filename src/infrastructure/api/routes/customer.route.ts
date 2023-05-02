d39c492333f103ebe0596dc78c928128b6439683
Leandro Bove <leandrobove@hotmail.com>
2023-05-02 12:19:38 -0300

    create customer endpoint implemented

---
+++ src/infrastructure/api/routes/customer.route.ts
@@ -0,0 +1,25 @@
+import express, { Request, Response } from "express";
+import CreateCustomerUseCase from "../../../usecase/customer/create/create.customer.usecase";
+import CustomerRepository from "../../customer/repository/sequelize/customer.repository";
+
+export const customerRoute = express.Router();
+
+customerRoute.post("/", async (req: Request, res: Response) => {
+    const useCase = new CreateCustomerUseCase(new CustomerRepository());
+    try {
+        const input = {
+            name: req.body.name,
+            address: {
+                street: req.body.address.street,
+                city: req.body.address.city,
+                number: req.body.address.number,
+                zip: req.body.address.zip
+            }
+        }
+
+        const output = await useCase.execute(input);
+        res.status(201).send(output);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+});
